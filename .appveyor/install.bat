16f6e10ad402088dd7bb094a4bc469d763d838fe
Christoph Auer <auer.christoph@outlook.at>
2017-02-16 12:10:57 +0100

    Install QtPackage for VS and setup gyp

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -1,11 +1,14 @@
 @echo off
 SET BUILD_DIR=C:\TBuild
 set LIB_DIR=%BUILD_DIR%\Libraries
+set SRC_DIR=%BUILD_DIR%\tdesktop
 SET QT_VERSION=5_6_2
 
 cd %BUILD_DIR%
 
 call:getDependencies
+call:installQT5Package
+call:setupGYP
 
 echo Finished!
 
@@ -24,4 +27,18 @@ GOTO:EOF
 	call prepare.bat
 	
 	dir
+GOTO:EOF
+
+:installQT5Package
+    call:logInfo "Installing Qt5Package for VS"
+	call powershell %SRC_DIR%\.appveyor\install-qt_package.ps1
+GOTO:EOF
+
+:setupGYP
+    call:logInfo "Setup GYP/Ninja and generate VS solution"
+    cd %LIB_DIR%
+	git clone https://chromium.googlesource.com/external/gyp
+    SET PATH=%PATH%;C:\TBuild\Libraries\gyp;C:\TBuild\Libraries\ninja;
+    cd ..\tdesktop\Telegram
+    call gyp\refresh.bat
 GOTO:EOF
