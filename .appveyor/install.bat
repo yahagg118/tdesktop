8b9c01ba2790d7e8004441a0a0b917ffeec88523
Christoph Auer <auer.christoph@outlook.at>
2017-02-18 11:52:01 +0100

    Test build matrix

--- .appveyor/install.bat
+++ .appveyor/install.bat
@@ -1,4 +1,6 @@
 @echo off
+@setlocal enableextensions enabledelayedexpansion
+
 SET BUILD_DIR=C:\TBuild
 set LIB_DIR=%BUILD_DIR%\Libraries
 set SRC_DIR=%BUILD_DIR%\tdesktop
@@ -8,10 +10,14 @@ cd %BUILD_DIR%
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
+call:configureBuild
+GOTO:EOF
+
 call:getDependencies
 call:setupGYP
 
 echo Finished!
+endlocal
 
 GOTO:EOF
 
@@ -24,7 +30,7 @@ GOTO:EOF
     call:logInfo "Clone dependencies repository"
     git clone -q --branch=master https://github.com/telegramdesktop/dependencies_windows.git %LIB_DIR%
     cd %LIB_DIR%
-	
+
 	call prepare.bat
 GOTO:EOF
 
@@ -35,4 +41,36 @@ GOTO:EOF
     SET PATH=%PATH%;C:\TBuild\Libraries\gyp;C:\TBuild\Libraries\ninja;
     cd ..\tdesktop\Telegram
     call gyp\refresh.bat
+GOTO:EOF
+
+:configureBuild
+    call:logInfo "Configuring build"
+	call:logInfo "Build version: %BUILD_VERSION%"
+	set GYP_DEFINES=
+
+	if not x"%BUILD_VERSION:disable_autoupdate=%"==x"%BUILD_VERSION%" (
+        set GYP_DEFINES=%GYP_DEFINES%,TDESKTOP_DISABLE_AUTOUPDATE
+    )
+
+	if not x"%BUILD_VERSION:disable_register_custom_scheme=%"==x"%BUILD_VERSION%" (
+        set GYP_DEFINES=%GYP_DEFINES%,TDESKTOP_DISABLE_REGISTER_CUSTOM_SCHEME
+    )
+
+	if not x"%BUILD_VERSION:disable_crash_reports=%"==x"%BUILD_VERSION%" (
+        set GYP_DEFINES=%GYP_DEFINES%,TDESKTOP_DISABLE_CRASH_REPORTS
+    )
+
+	if not x"%BUILD_VERSION:disable_network_proxy=%"==x"%BUILD_VERSION%" (
+        set GYP_DEFINES=%GYP_DEFINES%,TDESKTOP_DISABLE_NETWORK_PROXY
+    )
+
+	if not x"%BUILD_VERSION:disable_desktop_file_generation=%"==x"%BUILD_VERSION%" (
+        set GYP_DEFINES=%GYP_DEFINES%,TDESKTOP_DISABLE_DESKTOP_FILE_GENERATION
+    )
+
+	if not x"%BUILD_VERSION:disable_unity_integration=%"==x"%BUILD_VERSION%" (
+        set GYP_DEFINES=%GYP_DEFINES%,TDESKTOP_DISABLE_UNITY_INTEGRATION
+    )
+
+	call:logInfo "GYP Defines: %GYP_DEFINES%"
 GOTO:EOF
